0eb7a19eaeb50e55c03d3e4c5caae970677d7aee
nagymarci98 <55578294+nagymarci98@users.noreply.github.com>
2021-04-30 11:39:04 +0200

    Trying to start the action

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -9,7 +9,6 @@ import java.util.Random;
  */
 public class TorpedoStore {
 
-  // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; // NOSONAR
 
   private int torpedoCount = 0;
